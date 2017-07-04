24f1aa44f4efe410ef4d472ccfe2e782c31014bb
mmistroni <mmistroni@gmail.com>
2017-07-04 23:07:02 +0100

    Simple test for in memory db unit tests

--- src/test/scala/com/mm/sharesapp/persistence/SquerylPersistenceManagerTest.scala
+++ src/test/scala/com/mm/sharesapp/persistence/SquerylPersistenceManagerTest.scala
@@ -2,93 +2,126 @@ package com.mm.sharesapp.persistence
 
 import org.junit._
 import org.junit.Assert._
-import akka.actor.ActorSystem
-import akka.actor.{ ActorRef, Props, Terminated }
-import akka.actor.Actor
-import akka.actor.Props
-import akka.testkit.{ TestKit, TestActorRef, ImplicitSender, TestProbe }
-import scala.concurrent.duration._
-import scala.concurrent.Await
-import akka.pattern.ask
-import akka.util.Timeout
+import org.scalatest.{ BeforeAndAfterEach, BeforeAndAfterAll }
 import org.junit.runner.RunWith
 import org.scalatest.{ FlatSpecLike }
 import org.mockito._
 import org.mockito.Mockito._
-import com.mm.sharesapp.services.{SharePriceComponent, DataDownloaderComponent, SharePriceService}
+import com.mm.sharesapp.services.{ SharePriceComponent, DataDownloaderComponent, SharePriceService }
 import scala.util.Random
 import SharesSchema._
 import com.mm.sharesapp.entities._
-
 import org.junit.runner.RunWith
 import org.scalatest.FreeSpec
 import org.scalatest.Matchers
 import org.scalatest.junit.JUnitRunner
-
-import org.squeryl.{Session, SessionFactory}
-import org.squeryl.adapters.{MySQLAdapter,MySQLInnoDBAdapter}
+import org.squeryl.{ Session, SessionFactory, AbstractSession }
+import org.squeryl.adapters.{ MySQLAdapter, MySQLInnoDBAdapter }
 import org.squeryl.PrimitiveTypeMode._
-
 import org.squeryl.adapters.H2Adapter
-
-import org.squeryl.{AbstractSession, Session}
 import java.sql.Connection
 
-
-trait H2_Connection  {
-     Class.forName("org.h2.Driver")
-      
-      SessionFactory.concreteFactory = Some(() =>
-      Session.create(
-        java.sql.DriverManager.getConnection("jdbc:h2:mem:test",
-          "h2.user", "h2.password"),
-        new H2Adapter))
-  
+trait H2_ConnectionCommon extends DBConnector {
+  def connectToDbCommon(sessionFunc: Connection => AbstractSession): Option[() => AbstractSession] = {
+    {
+      Class.forName("org.h2.Driver")
+      Some(() => {
+        val c = java.sql.DriverManager.getConnection(
+          "jdbc:h2:mem:test",
+          "h2.user",
+          "h2.password")
+        sessionFunc(c)
+      })
     }
-
-
-/**
-trait H2_Connection extends BaseDBConnector with H2_ConnectionCommon {
-  def sessionCreator() : Option[() => AbstractSession] = connectToDbCommon(Session.create(_, ))
+  }
 }
-**/
 
+trait H2_Connection extends DBConnector with H2_ConnectionCommon {
+  def sessionCreator(): Option[() => AbstractSession] = connectToDbCommon(Session.create(_, new H2Adapter))
+}
 
-class TestData{
+class TestData {
   import SharesSchema._
 
   val sharePricesTests = sharePrices.insert(
-      SharePrice( new java.util.Date(), "TestTicker",
-                  -1.0, -2.0,
-                  -3.0, -4.0,
-                  -4.0, -5.0)) 
+    SharePrice(new java.util.Date(), "TestTicker",
+      -1.0, -2.0,
+      -3.0, -4.0,
+      -4.0, -5.0))
 }
 
 // Scrap this, Having a connection and a transaction we should be able to insert
 // data into the database
 
-
-
 @RunWith(classOf[JUnitRunner])
-class SquerylPersistenceServiceTest extends FreeSpec with Matchers with H2_Connection {
-                      
+class SquerylPersistenceServiceTest extends DbTestBase with Matchers with H2_Connection with BeforeAndAfterAll
+    with BeforeAndAfterEach {
+
+  def prePopulate = {
+    SharesSchema.create
+    val testData = new TestData()
+  }
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+
+    sessionCreator().foreach { _ =>
+      transaction {
+        SharesSchema.drop
+        
+      }
+    }
+
+  }
+
+  override def afterAll(): Unit = {
+    sessionCreator().foreach { _ =>
+      transaction {
+        SharesSchema.drop
+      }
+    }
+  }
+
   "The SquerylPersistenceManagerTest" - {
     "when calling insertShare with a Share object " - {
       "should insert and be able to find data in the database" in {
-       
-       transaction {
-         
-         val testData =  new TestData()
-         
-         // now retrieving it
-         val data = from(SharesSchema.sharePrices)(sharePrice => select(sharePrice)).toList
-         
-         data.size should be (1)
-         data(0).ticker should be ("TestTicker")
-         
-       }
-      }   
+
+        transaction {
+          // now retrieving it
+          prePopulate
+          val data = from(SharesSchema.sharePrices)(sharePrice => select(sharePrice)).toList
+
+          //data.size should be(1)
+          //data(0).ticker should be("TestTicker")
+
+        }
+      }
     }
   }
 
 }
+
+abstract class DbTestBase extends FreeSpec with BeforeAndAfterAll with BeforeAndAfterEach with Matchers
+    with H2_Connection {
+
+  def isIgnored(testName: String) =
+    sessionCreator().isEmpty || ignoredTests.exists(_ == testName)
+
+  def ignoredTests: List[String] = Nil
+
+  override def beforeAll() = {
+    val c = sessionCreator()
+    if (c.isDefined) {
+      SessionFactory.concreteFactory = c
+    }
+  }
+
+  override protected def runTest(testName: String, args: org.scalatest.Args): org.scalatest.Status = {
+    if (isIgnored(testName))
+      org.scalatest.SucceededStatus
+    else
+      super.runTest(testName, args)
+  }
+
+}
+
